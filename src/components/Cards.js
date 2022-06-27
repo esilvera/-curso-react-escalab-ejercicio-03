49a7dbb807d46a90cf65ff35197308c4987f9382
esilvera <edwards.silvera@gmail.com>
2022-06-27 12:59:57 -0400

    [feat] Cards Components - Operador Corto Circuito &&

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,16 +1,12 @@
 import Card from './Card'
 
 const Cards = (props) => {
-// ---  Renderizado condicional ---
-  if (!props.cities) {
-    return <h1>No Hay Ciudades</h1>
-  }
 
   return (
-
+    // --- Renderizado condicional con Operador Corto Circuito && ---
     <div>
       {
-        props.cities.map(city => (
+        props.cities && props.cities.map(city => (
           <Card 
             key={city.id} max={city.main.temp_max} min={city.main.temp_min} name={city.name} img={city.weather[0].icon} 
             onClose={() => alert(city.name)}
