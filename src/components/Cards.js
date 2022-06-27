0b58a2a2a642d33eb089b7453af6917d1d4293b4
esilvera <edwards.silvera@gmail.com>
2022-06-27 13:12:02 -0400

    [feat] Cards Components - Operador Ternario ?

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,20 +1,11 @@
 import Card from './Card'
 
 const Cards = (props) => {
-
-  return (
-    // --- Renderizado condicional con Operador Corto Circuito && ---
-    <div>
-      {
-        props.cities && props.cities.map(city => (
-          <Card 
-            key={city.id} max={city.main.temp_max} min={city.main.temp_min} name={city.name} img={city.weather[0].icon} 
-            onClose={() => alert(city.name)}
-          />
-        ))
-      }
-    </div>
-  );
+    // --- Renderizado condicional con Operador Operador Ternario ? ---
+  return <div>
+    {props.cities ? props.cities.map((city) => <Card key={city.id} max={city.main.temp_max} min={city.main.temp_min} 
+    name={city.name} img={city.weather[0].icon} onClose={() => alert(city.name)} />) : <h1>No Hay Ciudades</h1>}
+  </div>;
 };
 
 export default Cards;
